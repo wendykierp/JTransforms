97808a4f87644ec322f3ce4c10df35865c1ecb38
imagingbook <wilbur@ieee.org>
2025-11-30 10:57:13 +0100

    Added module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.jtransforms {
+    requires org.visnow.jlargearrays;
+    requires java.logging;
+    exports org.jtransforms.dct;
+    exports org.jtransforms.dht;
+    exports org.jtransforms.dst;
+    exports org.jtransforms.fft;
+}
